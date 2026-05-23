546e7e4faf9012d02bd1687989dfe930cd8052eb
Kelvin-Mutugi <mutugikelvin316@gmail.com>
2026-05-23 13:54:49 +0300

    fixes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Linux!"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    name = None  # Start with no name by default
+    
+    if request.method == 'POST':
+        # Get the name from the form input fields
+        name = request.form.get('user_name')
+        
+    # Send the name variable back to the HTML template
+    return render_template('index.html', name=name)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
